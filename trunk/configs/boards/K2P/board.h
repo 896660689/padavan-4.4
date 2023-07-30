61f601518d539ad70f568226ed677b5c3fa8ccfc
896660689 <zhangxuan000@live.com>
2023-07-30 21:06:42 +0800

    Add files via upload

--- trunk/configs/boards/K2P/board.h
+++ trunk/configs/boards/K2P/board.h
@@ -1,11 +1,11 @@
-/* K2P */
+/* Phicomm PSG-K2p */
 
 #define BOARD_PID		"K2P"
-#define BOARD_NAME		"K2P"
-#define BOARD_DESC		"PHICOMM K2P Wireless Router"
-#define BOARD_VENDOR_NAME	"PHICOMM"
+#define BOARD_NAME		"Phicomm-K2p"
+#define BOARD_DESC		"Phicomm K2P Wireless Router"
+#define BOARD_VENDOR_NAME	"Phicomm Computer Inc."
 #define BOARD_VENDOR_URL	"http://www.phicomm.com/"
-#define BOARD_MODEL_URL		"http://www.phicomm.com/"
+#define BOARD_MODEL_URL		"http://www.phicomm.com/cn/index.php/Products/family_details/cateid/18/id/122.html"
 #define BOARD_BOOT_TIME		30
 #define BOARD_FLASH_TIME	120
 
@@ -19,3 +19,4 @@
 #define BOARD_NUM_ETH_LEDS		0
 #define BOARD_NUM_UPHY_USB3		0
 #define BOARD_USB_PORT_SWAP		0
+
--- trunk/configs/boards/K2P/board.mk
+++ trunk/configs/boards/K2P/board.mk
@@ -1,9 +1,10 @@
 ##################################################################
-# Board PID # Board Name       # PRODUCT # Note
+# Board PID # Board Name	# PRODUCT # Note
 ##################################################################
-# K2P       # K2P              # MT7621  #
+# PSG	# PHICOMM PSG-K2P	# MT7621 #
 ##################################################################
 
 CFLAGS += -DBOARD_K2P -DBOARD_MT7615_DBDC
 BOARD_NUM_USB_PORTS=0
 CONFIG_BOARD_RAM_SIZE=128
+
